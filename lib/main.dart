90f1757c73da8d12faf7c4a61074126cdb8121b9
OmarEidAwad <152607793+OmarEidAwad@users.noreply.github.com>
2026-04-07 15:40:10 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,5 @@ import 'package:market_app/market_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MarketApp());
+  runApp(const MarketApp()); 
 }
